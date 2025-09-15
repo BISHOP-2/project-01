bde2352f15b445b174ea6ff722b03b6fe6303a25
Job Maina <mwangimaina2@s.karu.ac.ke>
2025-09-15 18:50:41 +0300

    setup

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import find_packages,setup
+from typing import List
+HYPEN_E_DOT = "-e ."
+def get_requirements(file_path:str)-> list[str]:
+    '''this function will return a list of requirements'''
+    requirements = []
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.replace("\n"," ") for req in requirements]
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+    return requirements
+setup(
+name = "HP Project",
+version =  "1.0.0",
+author = "biggie",
+author_email= "jobmaina642@gmail.com",
+packages= find_packages(),
+install_requires = get_requirements("requirements.txt")
+
+)
+
+
