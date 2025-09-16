c97f3d0fec8f26fb4d4c8552711fd8610ac0c921
Job Maina <mwangimaina2@s.karu.ac.ke>
2025-09-16 20:27:47 +0300

    data_transformation

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -6,6 +6,8 @@ import pandas as pd
 
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
 @dataclass
 class DataIngestionConfig:
     train_data_path:str = os.path.join("artifacts","train.csv")
@@ -42,3 +44,5 @@ if __name__ == "__main__":
     train_data,test_data = obj.initiate_data_ingestion()
     print("Trainig data saved at :",train_data)
     print("test data saved at:",test_data)
+    data_transformation = DataTransformation()
+    data_transformation.initiate_data_transformation(train_data,test_data)
